890c6053cedcddd4af332849e61638f84f850ac1
18940 <18940646130@163.com>
2020-06-30 23:56:50 +0800

    在整合序列化器下，查询、添加、删除

--- bookapp/serializers.py
+++ bookapp/serializers.py
@@ -29,7 +29,7 @@ class BookModelSerializer(serializers.ModelSerializer):
         # fields = ("book_name", "price", "pic", "publish")
 
         # 直接查询所有字段
-        fields = "__all__"
+        # fields = "__all__"
 
         # 可以指定不展示哪些字段
         # exclude = ("is_delete", "create_time", "status")
@@ -66,4 +66,49 @@ class BookDeModelSerializer(serializers.ModelSerializer):
             raise exceptions.ValidationError("图书名含有敏感字")
         return value
 
+# 整合序列化器
+class BookModelSerializerV2(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        # fields应该填写哪些字段  应该填写序列化与反序列化字段的并集
+        fields = ("book_name", "price", "publish", "authors", "pic")
 
+        # 添加DRF所提供的校验规则
+        # 通过此参数指定哪些字段是参与序列化的  哪些字段是参与反序列化的
+        extra_kwargs = {
+            "book_name": {
+                "required": True,  # 设置为必填字段
+                "min_length": 3,  # 最小长度
+                "error_messages": {
+                    "required": "图书名是必填的",
+                    "min_length": "长度不够，太短啦~"
+                }
+            },
+            # 指定此字段只参与反序列化
+            "publish": {
+                "write_only": True
+            },
+            "authors": {
+                "write_only": True
+            },
+            # 指定某个字段只参与序列化
+            "pic": {
+                "read_only": True
+            }
+        }
+
+    def validate_book_name(self, value):
+        # 自定义用户名校验规则
+        if "1" in value:
+            raise exceptions.ValidationError("图书名含有敏感字")
+        return value
+
+    # 全局校验钩子  可以通过attrs获取到前台发送的所有的参数
+    def validate(self, attrs):
+
+        price = attrs.get("price", 0)
+        # 没有获取到 price  所以是 NoneType
+        if price > 90:
+            raise exceptions.ValidationError("超过设定的最高价钱~")
+
+        return attrs
--- bookapp/views.py
+++ bookapp/views.py
@@ -5,6 +5,10 @@ from rest_framework import status
 from bookapp.models import Book
 from .serializers import BookModelSerializer  # 导入序列化
 from .serializers import BookDeModelSerializer  # 导入反序列化
+from .serializers import BookModelSerializerV2  # 导入整合序列化器
+
+
+# 在序列化器与反序列化器  分离情况下的类试图
 
 class BookAPIView(APIView):
 
@@ -15,7 +19,7 @@ class BookAPIView(APIView):
         if book_id:
 
             book_obj = Book.objects.get(pk=book_id)  # 获取id
-            book_ser = BookModelSerializer(book_obj).data   # 转化格式
+            book_ser = BookModelSerializer(book_obj).data  # 转化格式
             return Response({
                 "status": status.HTTP_200_OK,
                 "message": "查询单个图书成功",
@@ -74,4 +78,82 @@ class BookAPIView(APIView):
             "message": "添加图书成功",
             # 当群增多个时，无法序列化多个对象到前台  所以报错
             "result": BookDeModelSerializer(book_obj, many=many).data
+        })
+
+
+# 在序列化器与反序列化器 整合情况下的类试图
+class BookAPIViewV2(APIView):
+
+    # 查询信息
+    def get(self, request, *args, **kwargs):
+        book_id = kwargs.get("id")
+        if book_id:
+            print(book_id)
+            book_obj = Book.objects.get(pk=book_id, is_delete=False)
+            print(book_obj, type(book_obj), "1111")
+            book_ser = BookModelSerializerV2(book_obj).data
+            return Response({
+                "status": status.HTTP_200_OK,
+                "message": "查询单个图书成功",
+                "results": book_ser
+            })
+
+        else:
+            book_list = Book.objects.filter(is_delete=False)
+            book_list_ser = BookModelSerializerV2(book_list, many=True).data
+            return Response({
+                "status": status.HTTP_200_OK,
+                "message": "查询所有图书成功",
+                "results": book_list_ser
+            })
+
+    # 添加信息
+    def post(self, request, *args, **kwargs):
+
+        request_data = request.data
+        if isinstance(request_data, dict):  # 代表增加的是单个图书
+            # 将前端发送过来的数据交给反序列化器进行校验
+            many = False
+        elif isinstance(request_data, list):  # 代表添加多个图书
+            many = True
+        else:
+            return Response({
+                "status": status.HTTP_400_BAD_REQUEST,
+                "message": "请求参数格式有误",
+            })
+
+        book_ser = BookModelSerializerV2(data=request_data, many=many)
+        # 校验数据是否合法 raise_exception：一旦校验失败 立即抛出异常
+        book_ser.is_valid(raise_exception=True)
+        book_obj = book_ser.save()
+
+        return Response({
+            "status": status.HTTP_200_OK,
+            "message": "添加图书成功",
+            # 当群增多个时，无法序列化多个对象到前台  所以报错
+            "result": BookModelSerializerV2(book_obj, many=many).data
+        })
+
+    # 删除信息
+    def delete(self, request, *args, **kwargs):
+
+        book_id = kwargs.get("id")
+        if book_id:
+            # 删除单个  也作为删除多个
+            ids = [book_id]
+        else:
+            # 删除多个
+            ids = request.data.get("ids")
+
+        # 判断传递过来的图书的id是否在数据库  且还未删除
+        response = Book.objects.filter(pk__in=ids, is_delete=False).update(is_delete=True)
+        if response:
+            return Response({
+                "status": status.HTTP_200_OK,
+                "message": "删除成功"
+            })
+
+        return Response({
+            "status": status.HTTP_400_BAD_REQUEST,
+            "message": "删除失败或图书不存在"
         })
